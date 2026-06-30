b5b8b7045881aff6975fb0b4842ca00e2887e3d2
shruti0101 <shrutiguptacu@gmail.com>
2026-06-30 14:29:58 +0530

    changes

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import AboutClient from "./AboutClient";
-
-export const metadata = {
-  title: "About Us – Leading Garbage Bag Manufacturer | Sangam Plastic Industries",
-  description: "Learn more about Sangam Plastic Industries – a trusted manufacturer and bulk supplier of premium quality garbage bags, committed to sustainable waste management and customer satisfaction.",
-};
-
-export default function AboutUsPage() {
-  return <AboutClient />;
-=======
 import ContactClient from "./AboutClient";
 
 export const metadata = {
@@ -18,5 +7,4 @@ export const metadata = {
 
 export default function AboutHero() {
   return <ContactClient />;
->>>>>>> 10a1ac2 (changes)
 }
