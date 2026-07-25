477a282e92d307dfc5917ec4ab0a9ebd0ceae090
manish kumar <manishkumarstudent8744@gmail.com>
2026-07-25 12:52:49 +0530

    Blog pg chnages

--- app/our-blogs/[slug]/page.jsx
+++ app/our-blogs/[slug]/page.jsx
@@ -100,10 +100,10 @@ export default async function BlogDetail({ params }) {
   <div className="absolute inset-0 bg-black/40 z-[1]"></div>
   
         <div className="relative  z-10 flex h-full items-center justify-center max-w-6xl mx-auto px-6">
-          <div className="text-blue-900 max-w-2xl bg-white p-2 rounded">
+          <div className="text-blue-900 max-w-6xl bg-white p-2 rounded">
        
 
-            <h1 className="text-3xl md:text-5xl font-bold leading-tight mt-5">
+            <h1 className="text-xl md:text-4xl font-bold leading-tight mt-15">
               {blog.title}
             </h1>
 
